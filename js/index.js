f9ef089dffcb53e75e8eefe0010ff2a6c719ced4
vgimonnet <valentin.gimonnet18@gmail.com>
2023-08-09 09:59:01 +0200

    add google recaptcah and send mail through EmailJs

--- js/index.js
+++ js/index.js
@@ -22,19 +22,69 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // Form submit management - Send mail
   // const form = document.getElementById('form');
-  document.getElementById('form').addEventListener('submit', (e) => {
-    e.preventDefault();
-    const formData = Object.fromEntries(new FormData(e.target));
+  // document.getElementById('form').addEventListener('submit', (e) => {
+  //   e.preventDefault();
+  //   const formData = Object.fromEntries(new FormData(e.target));
 
-    const subject = formData.subject;
+  //   // const subject = formData.subject;
+  //   const society = formData?.society ? ` (${ formData?.society })` : '';
+  //   // const message = `
+  //   //   Nouvelle demande de contact de la part de ${ formData.name } ${ society } \n
+  //   //   Email : ${ formData.email } \n
+  //   //   Objet de la demande : ${ subject } \n
+  //   //   Message : \n
+  //   //   ${ formData.message }
+  //   // `;
+
+
+  //   // TODO: implement mail sending
+  //   // https://www.emailjs.com/docs/rest-api/send/
+  //   const data = {
+  //     service_id: 'service_y15exl9',
+  //     template_id: 'template_d1t7nmu',
+  //     user_id: 'GcO3YN83rCIefi_d4',
+  //     template_params: {
+  //         'name': formData.name,
+  //         society,
+  //         'message': formData.message,
+  //         'subject': formData.subject,
+  //         'g-recaptcha-response': '03AHJ_ASjnLA214KSNKFJAK12sfKASfehbmfd...'
+  //     }
+  //   };
+
+  //   console.log(data);
+  // });
+
+
+  onSubmit = (token) => {
+    const formData = Object.fromEntries(new FormData(document.getElementById('form')));
     const society = formData?.society ? ` (${ formData?.society })` : '';
-    const message = `
-      Nouvelle demande de contact de la part de ${ formData.name } ${ society } \n
-      Email : ${ formData.email } \n
-      Objet de la demande : ${ subject } \n
-      Message : \n
-      ${ formData.message }
-    `;
 
-  });
+    const data = {
+      service_id: 'service_y15exl9',
+      template_id: 'template_d1t7nmu',
+      user_id: 'GcO3YN83rCIefi_d4',
+      template_params: {
+          'name': formData.name,
+          society,
+          'message': formData.message,
+          'subject': formData.subject,
+          'g-recaptcha-response': token,
+      }
+    };
+
+    fetch('https://api.emailjs.com/api/v1.0/email/send', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+    .then(response => {
+      alert('Mail envoyé avec succès.');
+    })
+    .catch(error => {
+      alert('Echec de l\'envoi du mail. Vous pouvez réessayer ou bien nous contacter directement depuis notre adresse mail.');
+    })
+  }
 });
